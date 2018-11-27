7f77e21c073e5d532f29d743bf7fb8707f46c1b8
ost96 <37448385+ost96@users.noreply.github.com>
2018-11-27 12:51:09 +0100

    Initial commit, 8 tests

---
+++ src/main/java/app/Algo.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.lang.Math;
+import java.math.BigInteger;
+import java.util.function.BiFunction;
+
+public class Algo {
+    // returns true if a and b are coprime and false otherwise
+    public static boolean euclidesAreCoprime(BigInteger a, BigInteger b) throws IllegalArgumentException {
+            return false;
+    }
+
+    // returns the GCD of a and b
+    public static BigInteger euclidesGCD(BigInteger a, BigInteger b) {
+        BigInteger f = new BigInteger("0");
+        return f;
+    }
+
+}
---
+++ src/test/java/app/AlgoTest.java
@@ -0,0 +1,99 @@
+package app;
+
+import app.Algo;
+import java.lang.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
+
+public class AlgoTest {
+    @Test
+    public void euclidesAreCoprimeTest_primes() {
+        // prime
+        BigInteger a = new BigInteger("10000019");
+        // prime
+        BigInteger b = new BigInteger("10002347");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void euclidesAreCoprimeTest_pseudoprimeCoprimes() {
+        // pseudoprime (3 * 43 * 257)
+        BigInteger a = new BigInteger("33153");
+        // pseudoprime (151 * 331)
+        BigInteger b = new BigInteger("49981");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+        assertTrue(result);
+    }
+
+    @Test
+    public void euclidesAreCoprimeTest_notCoprimes() {
+        // (3 * k)
+        BigInteger a = new BigInteger("100");
+        // pseudoprime (3 * 43 * 257)
+        BigInteger b = new BigInteger("50");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void euclidesAreCoprimeTest_primesWithNegative() {
+        // negative prime
+        BigInteger a = new BigInteger("-10000019");
+        // prime
+        BigInteger b = new BigInteger("10002347");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void euclidesAreCoprimeTest_pseudoprimeCoprimesWithNegative() {
+        // negative pseudoprime (3 * 43 * 257)
+        BigInteger a = new BigInteger("-33153");
+        // pseudoprime (151 * 331)
+        BigInteger b = new BigInteger("49981");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void euclidesAreCoprimeTest_notCoprimesWithNegative() {
+        // negative number (3 * k)
+        BigInteger a = new BigInteger("-10000021");
+        // pseudoprime (3 * 43 * 257)
+        BigInteger b = new BigInteger("33153");
+        boolean result = Algo.euclidesAreCoprime(a, b);
+
+        assertTrue(result);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void euclidesAreCoprimeTest_withZero() {
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = new BigInteger("4");
+
+        exception.expect(IllegalArgumentException.class);
+        Algo.euclidesAreCoprime(a, b);
+    }
+
+    @Test
+    public void euclidesGCD_knownResult () {
+        BigInteger a = new BigInteger("12");
+        BigInteger b = new BigInteger("3");
+
+        BigInteger result = Algo.euclidesGCD(a, b);
+        assertEquals(new BigInteger("3"), result);
+    }
+}
