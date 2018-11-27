b0b6f2e57c4cf3152753a6ae6121c78c52678e0f
ost96 <37448385+ost96@users.noreply.github.com>
2018-11-27 13:29:24 +0100

    first iteration finished

--- src/main/java/app/Algo.java
+++ src/main/java/app/Algo.java
@@ -7,13 +7,18 @@ import java.util.function.BiFunction;
 public class Algo {
     // returns true if a and b are coprime and false otherwise
     public static boolean euclidesAreCoprime(BigInteger a, BigInteger b) throws IllegalArgumentException {
+        if (a.equals(BigInteger.ZERO) || b.equals(BigInteger.ZERO))
+            throw new IllegalArgumentException("arguments cannot be equal to 0");
+        else if (euclidesGCD(a, b).compareTo(BigInteger.ONE) == 0)
+            return true;
+        else
             return false;
     }
 
     // returns the GCD of a and b
+    // returns the GCD of a and b
     public static BigInteger euclidesGCD(BigInteger a, BigInteger b) {
-        BigInteger f = new BigInteger("0");
-        return f;
+        return a.gcd(b);
     }
 
 }
